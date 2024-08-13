e3140400645a952f48bcd19e30478c558d420f1c
Siddharth <sfjazwipropax52@outlook.com>
2024-08-14 01:42:35 +0530

    first java

---
+++ Fir.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Fir{
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(100);
+      System.out.println(al);
+
+    }
+}
